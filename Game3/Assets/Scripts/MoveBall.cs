fdd26f3b89786501bcf3e50b3798f1d0a2ed4c3e
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-02 15:00:25 -0500

    moving the ball

--- Game3/Assets/Scripts/MoveBall.cs
+++ Game3/Assets/Scripts/MoveBall.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveBall : MonoBehaviour
-
 {
-    [SerializeField] MoveBall ball;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 0.5f;
 
-    // Update is called once per frame
+
     void Update()
     {
-        Vector2 ball = new Vector2(5f, transform.position.y) ;
-        transform.position = ball;
+        Move();
+
+    }
+
+    public void Move()
+    {
+        var move = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var newpos = transform.position.x + move;
+        transform.position = new Vector2(newpos, transform.position.y);
     }
 }
+
+
