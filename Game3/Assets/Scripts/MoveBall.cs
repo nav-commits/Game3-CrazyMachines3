dd6fb73ee3de3c37b65772b8f8b63c520fcdb450
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-02 13:17:58 -0500

    fixing up UI componets, adding ball movement

---
+++ Game3/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+
+{
+    [SerializeField] MoveBall ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 ball = new Vector2(5f, transform.position.y) ;
+        transform.position = ball;
+    }
+}
